25a918adbc176b1c03a5ee8bb77852f37e0df67c
Joan <joanmuhingo@gmail.com>
2023-10-29 19:26:17 +0300

    changes on the code

--- src/index.js
+++ src/index.js
@@ -5,70 +5,73 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             console.log(data);
             const firstFilm = data[0];
-            displayFirstFilm (firstFilm);
-            displayFilmMenu(data);
+            displayFirstFilm(firstFilm);
+            displayAvailableTickets(data);
             availableTickets(data);
-            
+            displayFilmMenu(data);
         })
         .catch(error => {
             console.error("Error fetching data:", error);
         });
-        buyTicket();
 });
 
-function displayFirstFilm(film){
-    const infoBox= document.getElementById('showing');
+function availableTickets(film) {
+    let capacity = film.capacity;
+    let ticketsSold = film.tickets_sold;
+    return capacity - ticketsSold;
+}
+
+function displayAvailableTickets(films) {
+    const infoBox = document.getElementById('showing');
+        films.forEach(film => {
+            const available = availableTickets(film);
+            const ticketsOnSale = document.createElement('p');
+            ticketsOnSale.textContent = `Available Tickets: ${available}`;
+            infoBox.appendChild(ticketsOnSale);
+        });
+    };
+
+function displayFirstFilm(film) {
+    const poster = document.getElementById('poster');
     const filmTitle = document.getElementById('title');
     const runTime = document.getElementById('runtime');
+    const description = document.getElementById('film-info');
     const showTime = document.getElementById('showtime');
-    const availableTickets = document.getElementById('ticket-num');
+    const ticketsAvailable = document.getElementById('ticket-num');
 
-    const poster = document.createElement ('img');
     
-    poster.src = film.poster; 
-   filmTitle.textContent = film.title;
-   runTime.textContent = film.runtime;
-   showTime.textContent = film.showtime;
+    poster.src= film.poster;
+    filmTitle.textContent = film.title;
+    runTime.textContent = film.runtime;
+    description.textContent = film.description;
+    showTime.textContent = film.showtime;
 
-   availableTickets.innerHTML = '';
-   availableTickets();
-  infoBox.appendChild(poster);
-};
-displayFirstFilm();
+    const availableTicketsCount = availableTickets(film);
+    ticketsAvailable.textContent = `Available Tickets: ${availableTicketsCount}`;
+}
 
 function displayFilmMenu(films){
     let filmList = document.getElementById('films');
     filmList.innerHTML ='';
     for (const film of films){
-        films.innerHTML += '<li>' + film.title +'</li>';
+        filmList.innerHTML += '<li>' + film.title +'</li>';
     }
-
+ 
 }
-
-displayFilmMenu();
-
 function buyTicket(){
-    const filmBox = document.getElementById('showing');
-  const buyTicket = document.getElementById('buy-ticket');
-  const remTickets = document.getElementById('ticket-num');
-  remTickets.textContent = availableTickets();
-  let tickets = remTickets.textContent ;
-  buyTicket.addEventListener('onClick',function(e){
+const button = document.getElementById('buy-ticket');
+button.addEventListener('click'),function(e){
     e.preventDefault();
-    if(tickets <= 0){
-        alert('tickets are sold out.');
+    const ticketsAvailable = document.getElementById('ticket-num');
+    const availableTicketsCount = availableTickets(film);
+    if( availableTicketsCount <= 0){
+        alert('tickets are sold out');
     }
     else{
-       const ticketsOnSale = document.createElement('P');
-       ticketsOnSale.textContent = tickets;
-       filmBox.appendChild(ticketsOnSale);
-    }
-    
-  });
-}
+    availableTicketsCount--;
+    ticketsAvailable.textContent = `Available Tickets: ${availableTicketsCount}`;
 
-function availableTickets(film){
-    let capacity = film.capacity;
-    let ticketsSold = film.tickets_sold;
-    return capacity - ticketsSold;
+    }
+
+}
 }
